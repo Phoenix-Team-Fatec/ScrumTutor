879892bc187b78009c79779ab26414f83fa5ceb2
samekmd <samuelalkmc@gmail.com>
2023-10-05 07:10:34 -0300

    UPDATE EVENTOS
    Cria a página eventos, adiciona rotas para ele no flask.

--- SPRINT 2/app.py
+++ SPRINT 2/app.py
@@ -6,5 +6,9 @@ app = Flask("__name__")
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/Eventos')
+def Eventos():
+    return render_template('eventos.html')
+
 
 app.run(debug=True)
