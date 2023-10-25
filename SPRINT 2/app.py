cd00383acd22d0161d1e6741444a112257bab7c4
Vinicius <viniassisperetta@gmail.com>
2023-10-25 09:07:59 -0300

    Implementação do funcionamento do form da PACER

--- SPRINT 2/app.py
+++ SPRINT 2/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
+class avaliacao:
+    def __init__(self, papel, proatividade, autonomia, colaboracao, entrega):
+        self.papel = papel
+        self.proatividade = proatividade
+        self.autonomia = autonomia
+        self.colaboracao = colaboracao
+        self.entrega = entrega
 
 app = Flask("__name__")
 
+app.secret_key = 'phoenix'
+
+notas = []
 
 @app.route('/')
 def home():
@@ -11,9 +22,39 @@ def home():
 def Eventos():
     return render_template('eventos.html')
 
-@app.route('/Pacer', methods = ['POST', 'GET'])
+@app.route('/Pacer')
 def pacer():
-    return render_template('pacer.html')
+    return render_template('pacer.html', listas = notas)
+
+
+@app.route('/Criar', methods=['POST',])
+def criar():
+    papel = 'P.O'
+    proatividade = request.form['proat_po']
+    autonomia = request.form['aut_po']
+    colaboracao = request.form['colab_po']
+    entrega = request.form['entrega_po']
+    po = avaliacao(papel, proatividade, autonomia, colaboracao, entrega)
+    notas.append(po)
+
+    papel = 'S.M'
+    proatividade = request.form['proat_sm']
+    autonomia = request.form['aut_sm']
+    colaboracao = request.form['colab_sm']
+    entrega = request.form['entrega_sm']
+    sm = avaliacao(papel, proatividade, autonomia, colaboracao, entrega)
+    notas.append(sm)
+
+    papel = 'D.T'
+    proatividade = request.form['proat_dt']
+    autonomia = request.form['aut_dt']
+    colaboracao = request.form['colab_dt']
+    entrega = request.form['entrega_dt']
+    dt = avaliacao(papel, proatividade, autonomia, colaboracao, entrega)
+    
+    notas.append(dt)
+    return redirect('/Pacer')
+
 
 @app.route('/artefatos')
 def artefatos():
@@ -30,7 +71,5 @@ def apendice():
 @app.route('/Bibliografia')
 def bibliografia():
     return render_template('bibliografia.html')
-
-
 
 app.run(debug=True)
